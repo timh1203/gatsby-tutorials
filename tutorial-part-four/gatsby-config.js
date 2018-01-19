be0afce900aa9925d0ad75efeab04f93320b32a7
Timothy <timh1203@gmail.com>
2018-01-19 07:19:11 -0500

    Add tutorial files

---
+++ tutorial-part-four/gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+    author: `Timothy Hoang`,
+  },
+  plugins: [
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-glamor`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+  ],
+};
---
+++ tutorial-part-four/src/pages/pandas-and-bamboo.md
@@ -0,0 +1,8 @@
+---
+title: Pandas and Bamboo
+date: "2017-08-21"
+---
+
+Do they love Bamboo? No not really!
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4SZl1r2O_bY" frameborder="0" allowfullscreen></iframe>
